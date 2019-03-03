329308f2e522c9010d2ccba2f0e5379d57874c07
yangxiao <1249886841@qq.com>
2019-03-04 00:06:56 +0800

    多线程安全单例

--- src/ThreadSingleton.java
+++ src/ThreadSingleton.java
@@ -5,5 +5,23 @@
  */
 public class ThreadSingleton {
     //声明一个私有化的单例
-    private ThreadSingleton threadSingleton;
+    private volatile static ThreadSingleton threadSingleton = null;
+
+    //私有化构造函数
+    private ThreadSingleton(){
+
+    }
+
+    //提供一个获取实例的方法
+    public  static ThreadSingleton getInstance(){
+         //双重加锁
+         if(threadSingleton == null) {
+             synchronized (ThreadSingleton.class) {
+                 if(threadSingleton == null) {
+                     threadSingleton = new ThreadSingleton();
+                 }
+             }
+         }
+         return threadSingleton;
+    }
 }
